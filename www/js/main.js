a2dcbc23f9146284fa50b75decfb54584b141870
Joe Madich <joe@devcorr.com>
2017-05-06 13:00:17 -0400

    centering map on easton and setting zoom level

--- www/js/main.js
+++ www/js/main.js
@@ -3,8 +3,8 @@ function initMap() {
     map = new google.maps.Map(
         document.getElementById('map'),
         {
-            center: {lat: -34.397, lng: 150.644},
-            zoom: 8
+            center: {lat: 40.0507247, lng: -82.9175626},
+            zoom: 18
         }
     );
 }
