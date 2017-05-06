fc2d584f78553beb65ebd5ea55e9da09837d3810
Josh Green <josh@softwareverde.com>
2017-05-06 14:20:14 -0400

    Post request to endpoint.

--- www/js/main.js
+++ www/js/main.js
@@ -12,6 +12,18 @@ function initMap() {
     );
 }
 
+function postJson(url, json, callback) {
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: JSON.stringify(json),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: callback,
+        failure: callback
+    });
+}
+
 $(function() {
     $("#datepicker").datetimepicker({
         dateFormat: "yy-mm-dd",
@@ -19,6 +31,24 @@ $(function() {
         stepHour: 1,
         stepMinute: 5,
         stepSecond: 60
+    });
+
+    $("#control-submit").on("click", function() {
+        var date = $("#datepicker").val().replace(" ", "T") + "-0500";
+
+        postJson(
+            "http://smrtsandbox.com:8080/rest/easton-parking-data/getTrafficCounts",
+            {
+                "apiKey": "cd914c64-08e2-4245-84a9-8d767a299425",
+                "sensor_group": "",
+                "sensor_zone": "31 Fenlon St S",
+                "start_time": "2017-01-01T00:00:00-0500",
+                "end_time": "2017-01-31T23:00:00-0500"
+            },
+            function(data) {
+                console.log(data);
+            }
+        );
     });
 });
 
