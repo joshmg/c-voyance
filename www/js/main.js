66046a5c39ffdc89a3d0c6916cd2b7172b4c0423
Josh Green <josh@softwareverde.com>
2017-05-06 17:04:16 -0400

    Color variation for occupancy.

--- www/js/main.js
+++ www/js/main.js
@@ -1,4 +1,5 @@
 var SENSOR_ZONES = {"31 Fenlon St S":"31 Fenlon Street South","06 Lifetime Fitn":"06 Lifetime Fitness Parking","22 Easton Loop E":"22 Easton Loop E","08 West Garage G":"08 West Garage Ground","24 Fenlon Street":"24 Fenlon Street North","18 Regent St N":"18 Regent Street North","25 Nordstom SE":"25 Nordstom South East","39 Macy Garage":"39 Macys New Parking Garage","04 Townsfair":"04 Townsfair Way West","21 NordstromLot1":"21 Nordstrom Lot # 1","32 The Strand E":"32 The Strand East","20 The Strand N":"20 The Strand North","17 Container Sto":"17 The Container Store Parking","38 The Strand W":"38 The Strand West","37 Townsfair Way":"37 Townsfair Way East","14 Regent Street":"14 Regent Street South","36 Easton Statio":"36 Easton Station East","10 West Garage R":"10 West Garage 1st Floor","07 Easton Statio":"07 Easton Station West","23 Easton Loop E":"23 Easton Loop E","02 Easton Sq Pl":"02 Easton Square Place","16 New Bond St":"16 New Bond Street","03 Crate&Barrel":"03 Crate and Barrel","01 Easton Sq Pl":"01 Easton Square Place","26 North Park W":"26 North Parking Garage West","28 Macy E Lot N":"28 Macys Parking East Lot North","33 Garage E L1":"33 Parking Garage East Level 1","27 North Park E":"27 North Parking Garage East","19 NordstromLotS":"19 Nordstrom Lot South","35 Garage E L2":"35 Parking Garage East Level 2","11 West Garage L":"11 West Garage 1st Floor","34 Garage E L2":"34 Parking Garage East Level 2","15 Abuelos Lot":"15 Abuelos Parking Lot","13 HHGregg Front":"13 HH Gregg Front","05 Trader Joes":"05 Trader Joes Parking","12 HH Gregg Side":"12 HH Gregg Side"};
+var RECTANGLES = [];
 
 var map;
 function initMap() {
@@ -12,6 +13,41 @@ function initMap() {
             zoom: 18
         }
     );
+}
+
+function hsv2rgb(h, s, v) {
+  // adapted from http://schinckel.net/2012/01/10/hsv-to-rgb-in-javascript/
+  var rgb, i, data = [];
+  if (s === 0) {
+    rgb = [v,v,v];
+  } else {
+    h = h / 60;
+    i = Math.floor(h);
+    data = [v*(1-s), v*(1-s*(h-i)), v*(1-s*(1-(h-i)))];
+    switch(i) {
+      case 0:
+        rgb = [v, data[2], data[0]];
+        break;
+      case 1:
+        rgb = [data[1], v, data[0]];
+        break;
+      case 2:
+        rgb = [data[0], v, data[2]];
+        break;
+      case 3:
+        rgb = [data[0], data[1], v];
+        break;
+      case 4:
+        rgb = [data[2], data[0], v];
+        break;
+      default:
+        rgb = [v, data[0], data[1]];
+        break;
+    }
+  }
+  return '#' + rgb.map(function(x){
+    return ("0" + Math.round(x*255).toString(16)).slice(-2);
+  }).join('');
 }
 
 function drawRectangle(trafficCount) {
@@ -30,17 +66,32 @@ function drawRectangle(trafficCount) {
     bounds.west -= westOffset;
     bounds.east -= westOffset;
 
+    var assumedScale = 10; // Assuming scale is between 0 and 10.
+
+    var occupancy = parseFloat(trafficCount.occupancy) * assumedScale;
+
+    var hue = Math.floor((100 - occupancy) * 120 / 100);  // go from green to red
+    var saturation = Math.abs(occupancy - 50)/50;   // fade to white as it approaches 50
+    var rgb = hsv2rgb(hue, saturation, 1);
+
     var rectangle = new google.maps.Rectangle({
-        strokeColor: "#FF0000",
+        strokeColor: rgb,
         strokeOpacity: 0.8,
         strokeWeight: 2,
-        fillColor: "#FF0000",
+        fillColor: rgb,
         fillOpacity: 0.35,
         map: map,
         bounds: bounds
     });
 
-    return rectangle;
+    RECTANGLES.push(rectangle);
+}
+
+function clearMap() {
+    while (RECTANGLES.length > 0) {
+        var rectangle = RECTANGLES.pop();
+        rectangle.setMap(null);
+    }
 }
 
 function postJson(url, json, callback) {
@@ -87,6 +138,8 @@ $(function() {
         // startTime = "2017-01-01T00:00:00-0500";
         // endTime = "2017-01-01T23:00:00-0500";
 
+        clearMap();
+
         // "http://smrtsandbox.com:8080/rest/easton-parking-data/getTrafficCounts"
         for (var sensorZone in SENSOR_ZONES) {
             var value = SENSOR_ZONES[sensorZone];
@@ -103,7 +156,7 @@ $(function() {
                     var trafficCounts = data.trafficCounts;
                     for (var i in trafficCounts) {
                         var trafficCount = trafficCounts[i];
-                        console.log(trafficCount);
+                        // console.log(trafficCount);
                         drawRectangle(trafficCount);
                     }
                 }
